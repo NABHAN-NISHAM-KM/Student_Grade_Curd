9863035eedd9e9413c04c05e7104d70c21959435
NABHAN-NISHAM-KM <nabhannisham5@gmail.com>
2025-11-21 02:55:47 +0530

    changes

--- database/migrations/2025_11_20_192227_create_subjects_table.php
+++ database/migrations/2025_11_20_192227_create_subjects_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mst_subject');
+        Schema::dropIfExists('mst_subjects');
     }
 };
--- database/migrations/2025_11_20_192234_create_students_table.php
+++ database/migrations/2025_11_20_192234_create_students_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('mst_student');
+        Schema::dropIfExists('mst_students');
     }
 };
